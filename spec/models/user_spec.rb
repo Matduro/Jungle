f66d83434d55f4e150c6f9243759bf2c4c93342a
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-05-03 23:26:12 +0000

    add user tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,105 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+      it "should validate when user has all required fields completed" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        expect(@user).to be_valid
+      end
+
+      it "should not be valid when the password and password confirmation do not match" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "hacker_man")
+        expect(@user).to_not be_valid
+      end
+
+      it "should not be valid without an email" do
+      @user = User.new(
+        first_name: "Mathieu",
+        last_name: "Rousseau", 
+        password: "password123", 
+        password_confirmation: "hacker_man")
+      expect(@user).to_not be_valid
+      end
+
+      it "should not be valid without a password & password confirmation" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau", 
+          email: "mrousseau@lhlabs.com")
+        expect(@user).to_not be_valid
+      end
+
+      it "should not be valid without a name" do
+        @user = User.new(
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        expect(@user).to_not be_valid
+      end
+
+      it "should not be valid if the password is not at least 8 characters" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "1234567", 
+          password_confirmation: "1234567")
+        expect(@user).to_not be_valid
+      end
+
+      it "should not validate if email already exists" do
+        @userA = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau", 
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        @userA.save
+        @userB = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        expect(@userB).to_not be_valid
+      end
+    end
+
+    describe '.authenticate_with_credentials' do
+      it "user can successfuly authenticate with the correct credentials" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        @user.save
+        user = User.authenticate_with_credentials("mrousseau@lhlabs.com", "password123")
+        expect(user).to eq(@user)
+      end
+
+      it "user cannot successfuly authenticate with incorrect credentials" do
+        @user = User.new(
+          first_name: "Mathieu",
+          last_name: "Rousseau",
+          email: "mrousseau@lhlabs.com", 
+          password: "password123", 
+          password_confirmation: "password123")
+        @user.save
+        user = User.authenticate_with_credentials("mrousseau@lhlabs.com", "hacker_man")
+        expect(user).to_not eq(@user)
+      end
+
+    end
+  end
